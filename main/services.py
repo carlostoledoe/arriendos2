c6f8c8a275fd7ded18683c227d558e863ca4637a
Carlos Toledo <carlombo@hotmail.com>
2024-08-16 17:20:44 -0400

    [ADD] Se crean funciones para editar modelos

--- main/services.py
+++ main/services.py
@@ -22,6 +22,26 @@ def crear_inmueble(nombre:str, descripcion:str, m2_construidos:int, m2_totales:i
     )
     return True
 
+def editar_inmueble(inmueble_id:int, nombre:str, descripcion:str, m2_construidos:int, m2_totales:int, num_estacionamientos:int, num_habitaciones:int, num_baños:int, direccion:str, precio_mensual_arriendo:int, tipo_de_inmueble:str, comuna:str, rut_propietario:str, imagen:object):
+    inmueble = Inmueble.objects.get(id=inmueble_id)
+    comuna = Comuna.objects.get(cod=comuna)
+    propietario = User.objects.get(username=rut_propietario)
+    inmueble.nombre = nombre
+    inmueble.descripcion = descripcion
+    inmueble.m2_construidos = m2_construidos
+    inmueble.m2_totales = m2_totales
+    inmueble.num_estacionamientos = num_estacionamientos
+    inmueble.num_habitaciones = num_habitaciones
+    inmueble.num_baños = num_baños
+    inmueble.direccion = direccion
+    inmueble.precio_mensual_arriendo = precio_mensual_arriendo
+    inmueble.tipo_de_inmueble = tipo_de_inmueble
+    inmueble.comuna = comuna
+    inmueble.propietario = propietario
+    inmueble.imagen = imagen
+    inmueble.save()
+    return True
+
 
 def crear_user(username:str, first_name:str, last_name:str, email:str, password:str, pass_confirm:str, direccion:str, rol:str='arrendatario', telefono:str=None) -> bool:
     if password != pass_confirm:
@@ -46,3 +66,34 @@ def crear_user(username:str, first_name:str, last_name:str, email:str, password:
     )
     # messages.success(request, 'Usuario creado con éxito! Por favor, ingrese')
     return True
+
+def eliminar_inmueble(inmueble_id):
+    inmueble_encontrado = Inmueble.objects.get(id=inmueble_id)
+    inmueble_encontrado.delete()
+    return True
+
+def eliminar_user(rut:str):
+    eliminar = User.objects.get(username=rut)
+    eliminar.delete()
+    return True
+
+
+def editar_user_sin_password(rut:str, first_name:str, last_name:str, email:str, direccion:str, rol:str, telefono:str=None):
+    user = User.objects.get(username=rut)
+    user.first_name = first_name
+    user.last_name = last_name
+    user.email = email
+    user.save()
+    user_profile = UserProfile.objects.get(user=user)
+    user_profile.direccion = direccion
+    user_profile.telefono_personal = telefono
+    user_profile.rol = rol
+    user_profile.save()
+
+def cambio_password(request, password:str, password_repeat:str):
+    if password != password_repeat:
+        # messages.error(request, 'Las contraseñas no coinciden')
+        return
+    request.user.set_password(password)
+    request.user.save()
+    #  messages.success(request, 'Contraseña actualizada exitosamente')
