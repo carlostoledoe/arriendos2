43a6955eb6a119915c1f724f079ad14051dc3253
Carlos Toledo <carlombo@hotmail.com>
2024-08-17 12:06:08 -0400

    [ADD] Se crea formulario login y se configuras las rutas para login y logout. También se creaó el archivo base.html, navbar y footer sin implementar

--- arriendos/settings.py
+++ arriendos/settings.py
@@ -133,3 +133,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login'
